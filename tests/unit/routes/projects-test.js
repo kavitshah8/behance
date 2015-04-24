9c30460a9554ad93196ec2ab87b2b1d4e0d20771
kavitshah8 <kavit.shah8@gmail.com>
2015-04-24 19:07:14 -0400

    navigation bar, home page

--- tests/unit/routes/projects-test.js
+++ tests/unit/routes/projects-test.js
@@ -1,19 +1,20 @@
 /* jshint expr:true */
 import { expect } from 'chai';
-import { describeModule, it } from 'ember-mocha';
+import { describeModule, it, visit } from 'ember-mocha';
 
 describeModule( 'route:projects', 'ProjectsRoute', function() {
 
   it('exists', function() {
-  	var route = this.subject();
-  	expect(route).to.be.ok;
+		var route = this.subject();
+		expect(route).to.be.ok;
   });
 
   it('testing model hoook', function() {
 		var route = this.subject();
 		var mock = sinon.mock( route );
 		mock.expects('model');
-		route.model();
+
+    route.model();
 		mock.verify();
 		mock.restore();
 	});
